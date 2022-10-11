f73d1d6bafcad8ce35c644b80438e65e3011cfad
Warren Anderson <3522283+wkande@users.noreply.github.com>
2022-10-11 10:41:56 -0600

    Fix tabs to use proper component properties (#22)

--- docs/_components/CopyIcon.vue
+++ docs/_components/CopyIcon.vue
@@ -2,19 +2,15 @@
 -->
 
 <template>
-  <!-- COPY ICON 
-    The style applied (style="opacity: 60%; width: 12px") is very
-    important. If included in the class it will work on the dev 
-    server but not on a production build.
-  -->
-  <div style="display: inline-block; width: 18px">
-    <img
+  <div style="display: inline-block; width: 38px">
+    <a
       :id="iconId"
-      v-on:click="copyText()"
-      src="/img/copy.png"
+      href="#"
       class="contract-addresses-copy-icon"
-      style="opacity: 60%; width: 12px"
-    />
+      v-on:click="copyText()"
+    >
+      ❏
+    </a>
   </div>
 </template>
 
@@ -28,16 +24,16 @@ export default {
   methods: {
     copyText() {
       var copyIcon = document.getElementById(this.iconId);
-      copyIcon.style.opacity = '100%';
-      copyIcon.style.width = '20px';
+      copyIcon.innerHTML = 'Copied';
+      copyIcon.style.fontSize = 'small';
       window.setTimeout(this.setCopiedTimeout, 700, copyIcon);
 
       if (navigator && navigator.clipboard && navigator.clipboard.writeText)
         navigator.clipboard.writeText(this.text);
     },
     setCopiedTimeout(element) {
-      element.style.opacity = '60%';
-      element.style.width = '12px';
+      element.style.fontSize = 'large';
+      element.innerText = '❏';
     },
   },
   mounted() {
@@ -52,8 +48,8 @@ export default {
 
 <style scoped>
 .contract-addresses-copy-icon {
-  margin-left: 5px;
-  cursor: pointer;
-  height: 11px;
+  font-size: small;
+  margin-left: 9px;
+  font-size: large;
 }
 </style>
