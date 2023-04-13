d2c9ec837c681cea7af9024c82f3dc16371dd463
Warren Anderson <warren@api3.org>
2023-04-13 05:42:17 -0600

    Reads new keys from versions.json

--- docs/_components/reference/VersionPicklist.vue
+++ docs/_components/reference/VersionPicklist.vue
@@ -2,20 +2,21 @@
   <span v-if="versions.length > 1">
     <select @change="goToRoute" v-model="path" class="api3-version-select">
       <option v-for="vrs in versions" :key="vrs.path" :value="vrs.path">
-        {{ vrs.version }}
+        <!--  Only show /next if in DEV (isDev) -->
+        <span v-if="isDev">{{ vrs.version }}</span>
         <!-- https://unicode-table.com/en/sets/arrow-symbols/#down-arrows -->
-        <span v-if="path === vrs.path">&#9660;</span>
+        <span v-if="path === vrs.path">&nbsp;&#9660;</span>
       </option>
     </select>
   </span>
+  <!-- This can be removed when OIS has more than one version -->
   <span v-else-if="versions.length === 1"> {{ versions[0].version }}</span>
 </template>
 
 <script>
-import versions from '../../.vitepress/versions';
+import versionsArray from '../../.vitepress/versions';
 import { useRouter, useData, useRoute } from 'vitepress';
 import { watch } from 'vue';
-import globalSearch from '../../.vitepress/theme/index.js';
 
 //https://github.com/vuejs/vue-router/issues/3379
 
@@ -23,6 +24,7 @@ export default {
   name: 'VersionPicklist',
   data: () => ({
     path: undefined,
+    isDev: undefined,
     versions: [], // Do not use undefined or the template wil error when loaded
     goRouterFunc: useRouter().go,
   }),
@@ -34,6 +36,7 @@ export default {
       this.parsePath('/' + p);
       this.setPickListData();
     });
+
     const { path } = useRoute();
     this.parsePath(path);
 
@@ -49,10 +52,23 @@ export default {
     },
     setPickListData() {
       if (this.path.indexOf('/reference/airnode/') > -1) {
-        this.versions = versions.versionsAirnode;
+        this.versions = versionsArray.versionsAirnode.slice(); // slice makes a copy of the org array
       } else if (this.path.indexOf('/reference/ois/') > -1) {
-        this.versions = versions.versionsOIS;
+        this.versions = versionsArray.versionsOIS.slice(); // slice makes a copy of the org array
       } else this.versions = [];
+      // Alter the version array for DEV vrs PROD
+      // For now OIS only has one version (this.versions.length > 1), can remove it later
+      if (
+        window.location.href.indexOf('localhost:') === -1 &&
+        this.versions.length > 1
+      ) {
+        // If PROD remove /next for Airnode and OIS
+        // The top of the array will always be the /next version.
+        this.versions.shift();
+      } else {
+        // If Dev the array stays as is
+        this.isDev = true;
+      }
     },
     goToRoute() {
       this.goRouterFunc(this.path);
