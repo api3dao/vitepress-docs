2fbb574843113db293cdd6e5bb095a2607e3d6cd
Warren Anderson <warren@api3.org>
2023-04-26 04:46:53 -0600

    Add legacy version to sidebar picklist Airnode and OIS only

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -28,6 +28,7 @@ export default {
   // injects the slots
   Layout: Api3Layout,
   enhanceApp({ app }) {
+    console.log('API3 documentation loading...');
     app.component('VersionPicklist', VersionPicklist);
     app.component('SearchBtn', SearchBtn);
     app.component('SearchResults', SearchResults);
