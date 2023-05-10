c3b0457d38262636b5b03ba622369c76c88bcca7
KenCarvAPI <ben@api3.org>
2023-05-10 14:15:07 +0100

    ga4_tag (#456)

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -8,6 +8,18 @@ export default {
   appearance: true,
   ignoreDeadLinks: true,
   head: [
+    [
+      'script',
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-D3LJ95MXN9',
+      },
+    ],
+    [
+      'script',
+      {},
+      "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-D3LJ95MXN9');",
+    ],
     ['link', { rel: 'stylesheet', href: '/api3.css' }],
     [
       'link',
