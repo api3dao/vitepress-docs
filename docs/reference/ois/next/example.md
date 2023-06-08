a930c4ccc07da9254a6afa9321346eb08ccdceef
Warren Anderson <warren@api3.org>
2023-06-08 11:42:47 -0600

    Create OIS `v2.1` files and adjust version support files

---
+++ docs/reference/ois/next/example.md
@@ -0,0 +1,24 @@
+---
+title: Example OIS Object
+sidebarHeader: Reference
+sidebarSubHeader: OIS
+pageHeader: Reference → OIS → v2.1
+path: /reference/ois/next/example.html
+version: v2.1
+outline: deep
+tags:
+---
+
+<VersionWarning/>
+
+<PageHeader/>
+
+<SearchHighlight/>
+
+<FlexStartTag/>
+
+# {{$frontmatter.title}}
+
+<<< @/reference/ois/next/example.json
+
+<FlexEndTag/>
---
+++ docs/reference/ois/next/sidebar.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    text: '',
+    collapsable: false,
+    items: [
+      { text: 'Understanding OIS', link: '/reference/ois/next/' },
+      { text: 'Versions', link: '/reference/ois/next/versions' },
+      { text: 'Specification', link: '/reference/ois/next/specification' },
+      {
+        text: 'Reserved Parameter',
+        link: '/reference/ois/next/reserved-parameters',
+      },
+      {
+        text: 'Pre/Post Processing',
+        link: '/reference/ois/next/processing',
+      },
+      {
+        text: 'Example OIS Object',
+        link: '/reference/ois/next/example',
+      },
+      {
+        text: 'OIS Template',
+        link: '/reference/ois/next/template',
+      },
+      /*
+      '/reference/ois/next/processing.md',
+      '/reference/ois/next/example.md',
+      '/reference/ois/next/ois-template.md',
+      */
+    ],
+  },
+];
