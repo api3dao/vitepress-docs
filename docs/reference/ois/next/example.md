60c9e8f7e0ca55782e034653be9f0ae705b91707
Warren Anderson <warren@api3.org>
2023-11-01 15:01:33 -0600

    create ios next

---
+++ docs/reference/ois/next/example.md
@@ -0,0 +1,24 @@
+---
+title: Example OIS Object
+sidebarHeader: Reference
+sidebarSubHeader: OIS
+pageHeader: Reference → OIS → v2.2
+path: /reference/ois/latest/example.html
+version: v2.2
+outline: deep
+tags:
+---
+
+<VersionWarning/>
+
+<PageHeader/>
+
+<SearchHighlight/>
+
+<FlexStartTag/>
+
+# {{$frontmatter.title}}
+
+<<< @/reference/ois/latest/example.json
+
+<FlexEndTag/>
---
+++ docs/reference/ois/next/sidebar.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    text: '',
+    collapsable: false,
+    items: [
+      { text: 'Understanding OIS', link: '/reference/ois/latest/' },
+      { text: 'Versions', link: '/reference/ois/latest/versions' },
+      { text: 'Specification', link: '/reference/ois/latest/specification' },
+      {
+        text: 'Reserved Parameter',
+        link: '/reference/ois/latest/reserved-parameters',
+      },
+      {
+        text: 'Pre/Post Processing',
+        link: '/reference/ois/latest/processing',
+      },
+      {
+        text: 'Example OIS Object',
+        link: '/reference/ois/latest/example',
+      },
+      {
+        text: 'OIS Template',
+        link: '/reference/ois/latest/template',
+      },
+    ],
+  },
+];
