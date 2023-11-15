20a9cfec25edc36a406aa5873ea42726ca5f0863
KenCarvAPI <ben@api3.org>
2023-11-15 19:58:57 +0300

    Google Analytics GA4 (#663)
    * UTM_81123
    
    * GA4UTM
    
    * GA4UTM
    
    * Update config.js
    
    Added function line termination.
    
    ---------
    
    Co-authored-by: TimesThreeDX <134428920+TimesThreeDX@users.noreply.github.com>
    Co-authored-by: Warren Anderson <3522283+wkande@users.noreply.github.com>

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -10,10 +10,8 @@ export default {
   head: [
     [
       'script',
-      {
-        async: true,
-        src: 'https://www.googletagmanager.com/gtag/js?id=G-D3LJ95MXN9',
-      },
+      {},
+      "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','GTM-PKWG7ZFR');",
     ],
     [
       'script',
