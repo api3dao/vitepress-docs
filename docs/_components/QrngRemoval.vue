e0989ef1fe9721af4431c7d82f1c98e5a4e3ec37
Warren Anderson <warren@api3.org>
2024-09-16 05:17:57 -0600

    css changes

--- docs/_components/QrngRemoval.vue
+++ docs/_components/QrngRemoval.vue
@@ -1,19 +1,34 @@
 <template>
-  <div
-    style="
-      margin-top: 10px;
-      margin-bottom: 10px;
-      padding: 15px;
-      border: 1px gray solid;
-      border-radius: 0.25em;
-      max-width: 650px;
-    "
-  >
-    Effective 1 January 2025, API3 will be sunsetting our QRNG service.
-    <a
-      href="https://x.com/API3DAO/status/1831820055312789650"
-      target="api3_qrng"
-      >Learn more</a
-    >
+  <div class="qrng-flex-container">
+    <img
+      src="/public/img/blue-warning3.png"
+      style="width: 22px; height: 22px"
+    />
+
+    <span>
+      &nbsp;As of January 1, 2025, API3 will no longer be supporting the QRNG
+      service.
+
+      <span style="white-space: nowrap"
+        ><a
+          style="color: #194aff"
+          href="https://x.com/API3DAO/status/1831820055312789650"
+          target="api3_qrng"
+          >Learn more</a
+        ></span
+      >
+    </span>
   </div>
 </template>
+<style scoped>
+.qrng-flex-container {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding: 15px;
+  border: 1px gray solid;
+  border-radius: 0.25em;
+  max-width: 850px;
+  display: flex;
+  flex-wrap: nowrap;
+}
+</style>
